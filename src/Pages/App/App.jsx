601bdbf52656202a88dd27ceb0863f79da23d66d
jdx <librededrogas420@gmail.com>
2023-09-29 16:08:11 -0500

    i changed style of navbar to keep fixed in screen

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -10,11 +10,11 @@ function App() {
     <BrowserRouter>
       <ContextMovieProvided>
         <NavBar/>
-        <Routes>
-          {routesList.map((route, index) => {
-            return (<Route key={index} path={route.path} element={route.renderElement} > {route.nesting.nest && route.nesting.list.map((routeItem, index)=>(<Route key={index+'nest'} path={routeItem.path} element={routeItem.renderElement} />))}</Route>)
-          })}
-        </Routes>
+          <Routes>
+            {routesList.map((route, index) => {
+              return (<Route key={index} path={route.path} element={route.renderElement} > {route.nesting.nest && route.nesting.list.map((routeItem, index)=>(<Route key={index+'nest'} path={routeItem.path} element={routeItem.renderElement} />))}</Route>)
+            })}
+          </Routes>
         <Footer/>
       </ContextMovieProvided>
     </BrowserRouter>
